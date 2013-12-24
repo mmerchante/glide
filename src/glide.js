30ca7c3a38a061b8d48543569c8f316991a8d88e
mmerchante <merchante.mariano@gmail.com>
2013-12-24 02:39:43 -0300

    + Uniform density.

--- src/glide.js
+++ src/glide.js
@@ -3,7 +3,7 @@ var windDirection = new THREE.Vector3( 0, 1, 0);
 var tolerance = .5;
 
 function render() {
-	sampleGeometry(baseGeometry);
+	sampleGeometry(baseGeometry, 2);
 	requestAnimationFrame(render);
 	renderer.render(scene, camera);
 }		
@@ -24,8 +24,8 @@ render();
 
 function clearScene(scene) {
 
-	var obj, i;
-	for ( i = scene.children.length - 1; i >= 0; i--) {
+	var obj;
+	for (var i = scene.children.length - 1; i >= 0; i--) {
 	    obj = scene.children[i];
 
 	    if (obj !== camera)
@@ -33,39 +33,49 @@ function clearScene(scene) {
 	}
 }
 
-function sampleGeometry(geometry, density) {
-
+function sampleGeometry(geometry, surfaceDensity) {
+	
 	clearScene(scene);	
 
 	geometry.faces.forEach(function(face) {	
 
 		// Only sample faces facing windDirection
 		if(face.normal.dot(windDirection) > 1 - tolerance * 2) {
-			var u = Math.random();
-			var v = Math.random();
 
-	        if (u + v > 1)
-	        {
-	            u = 1 - u;
-	            v = 1 - v;
-	        }
+			// Make sure big polys have enough samples (also, lol@cache coherency)
+			var ab = new THREE.Vector3().subVectors( geometry.vertices[face.b], geometry.vertices[face.a]);
+			var ac = new THREE.Vector3().subVectors( geometry.vertices[face.c], geometry.vertices[face.a]);
+			var area = new THREE.Vector3().crossVectors(ab, ac).length() * surfaceDensity / 2.0;
 
-	        var w = 1 - u - v;
+			for(var i = 0; i < area; i++) {
 
-        	// ugh...
-			var v1 = geometry.vertices[face.a].clone();
-			var v2 = geometry.vertices[face.b].clone();
-			var v3 = geometry.vertices[face.c].clone();
+		    	// ugh...
+				var v1 = geometry.vertices[face.a].clone();
+				var v2 = geometry.vertices[face.b].clone();
+				var v3 = geometry.vertices[face.c].clone();
 
-			var p = v1.multiplyScalar(u).addVectors(v2.multiplyScalar(v), v3.multiplyScalar(w));
+				var u = THREE.Math.random16();
+				var v = THREE.Math.random16();
 
-			// Debug thingies
-			var g = new THREE.SphereGeometry(.025, 4, 4);
-			var material = new THREE.MeshBasicMaterial({ color : 0xcc4444 });
-			var cube = new THREE.Mesh( g, material );
-			cube.position = p;
+		        if (u + v > 1)
+		        {
+		            u = 1 - u;
+		            v = 1 - v;
+		        }
 
-			scene.add( cube );
+		        var w = 1 - u - v;
+
+				var p = v1.multiplyScalar(u).addVectors(v2.multiplyScalar(v), v3.multiplyScalar(w));
+
+				// Debug thingies
+				var g = new THREE.SphereGeometry(.025, 4, 4);
+				var material = new THREE.MeshBasicMaterial({ color : 0xcc4444 });
+				var cube = new THREE.Mesh( g, material );
+				cube.position = p;
+
+				scene.add( cube );
+
+			}
 		}
 	});
 }
